6696a6c67197105240e79bbb217afc0ab3d2badb
Mathias Jesse <mathias.jesse@aau.at>
2020-06-05 13:27:54 +0200

    Added interfaces for the algorithms; Added interface for the management of data;

---
+++ recommendation/algorithm_interface.py
@@ -0,0 +1,9 @@
+class algorithm_interface:
+    def generate_recommendations(self, selectedMovie) -> list:
+
+        """
+        Function that should be implemented in the various algorithms so it generates recommendations based on the selected movie.
+        :param: selectedMovie: complete movie object that the user selected as a reference point
+        :return: returns a list of movies recommended by that algorithm
+        """
+        pass
